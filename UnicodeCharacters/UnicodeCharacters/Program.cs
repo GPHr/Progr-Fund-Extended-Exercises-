47ec3ee41a457f5dce2f8aed699ec24298c57c06
Georgi Hristov <32270329+GPHr@users.noreply.github.com>
2017-12-04 18:53:15 +0200

    Add files via upload

---
+++ UnicodeCharacters/UnicodeCharacters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+namespace UnicodeCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            var uniCode = string.Empty;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var code = $"{(int)text[i]:X4}".ToLower();
+                uniCode += $"\\u{code}";
+               //var chars = value
+               //    .Select(c => (int)c)
+               //    .Select(c => $@"\u{c:x4}");
+            }
+
+            Console.WriteLine(uniCode);
+        }
+    }
+}
