6497b2eddd489dd950bab5071a5e1fce9fad0895
Georgi Hristov <32270329+GPHr@users.noreply.github.com>
2017-12-04 18:44:56 +0200

    Add files via upload

---
+++ Problem1_Poke_Mon/Problem1_Poke_Mon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Problem1_Poke_Mon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int M = int.Parse(Console.ReadLine());
+            int Y = int.Parse(Console.ReadLine());
+            int newN = N;
+
+            int count = 0;
+            while (N>=M)
+            {
+                var C = N - M;
+                 
+                if (C == newN/2 && Y>0)
+                {
+                    C = C / Y;
+                   }
+                N = C;
+                count++;
+                
+            }
+            Console.WriteLine(N);
+            Console.WriteLine(count);
+        }
+    }
+}
